689f3df97a19f7a11add5ca198d47a781771c498
Vishant Bhardwaj <bhardwajvishant52@gmail.com>
2025-12-25 18:06:53 +0530

    second commit

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
-const JWT_ADMIN_PASSWORD = "adminkeliyesigninkrnekatarika";
-const JWT_USER_PASSWORD = "userkeliyesiginkrnekatarika";
+const JWT_ADMIN_PASSWORD = process.env.JWT_ADMIN_PASSWORD;
+const JWT_USER_PASSWORD = process.env.JWT_USER_PASSWORD;
 
 module.exports = {
-    JWT_ADMIN_PASSWORD,
-    JWT_USER_PASSWORD
+  JWT_ADMIN_PASSWORD,
+  JWT_USER_PASSWORD
 };
