81d808cf3fd5c34e4f73e2df83d67c2610a4ba19
Vishant Bhardwaj <bhardwajvishant52@gmail.com>
2026-03-31 00:21:03 +0530

    fix admin dashboard loading

--- middleware/admin.js
+++ middleware/admin.js
@@ -6,8 +6,15 @@ const {JWT_ADMIN_PASSWORD} = require("../config");
 
 // Define the adminMiddleware function to verify the admin token
 function adminMiddleware(req,res,next){
+    const authHeader = req.headers.authorization; // ✅ FIX
+
+    if (!authHeader) {
+        return res.status(401).json({
+            message: "Token missing"
+        });
+    }
     // Retrieve the authorization token from the request headers
-    const token = req.headers.token;
+    const token = authHeader.split(" ")[1];
 
     // Use a try-catch block to handle errors that may occur during token verification
     try{
